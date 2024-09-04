9dcb1345ff710cf1cd6ca71221f5d84a14ba5899
jai <jaix2793@gmail.com>
2024-09-04 20:07:08 +0530

    updates

--- src/components/ImageEditor.jsx
+++ src/components/ImageEditor.jsx
@@ -1,24 +1,24 @@
 // src/components/ImageEditor.js
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
-
-import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
-
-// const style = {
-//     position: 'absolute',
-//     top: '50%',
-//     left: '50%',
-//     transform: 'translate(-50%, -50%)',
-//     width: '70vw', // Increased width for better visibility
-//     bgcolor: 'background.paper',
-//     border: '2px solid #000',
-//     boxShadow: 24,
-//     p: 4,
-//     overflow: 'auto', // Ensure content inside the modal can scroll if necessary
-// };
+import { useForm } from 'react-hook-form';
+import { toast, Toaster } from 'sonner';
+import { ClipLoader } from "react-spinners";
+import axios from 'axios';
 
 const ImageEditor = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [publicImages, setPublicImages] = useState(null)
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm()
+
+
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
@@ -28,8 +28,17 @@ const ImageEditor = () => {
 
     const [previewImage, setPreviewImage] = useState(null)
 
-    const { getRootProps, getInputProps } = useDropzone({
-        accept: 'image/*',
+    const getData = () => {
+        axios.get(`${import.meta.env.VITE_API_URL}/game`).then((res) => {
+            setPublicImages(res.data.result)
+        }).catch(err => console.log(err))
+    }
+
+
+    const { acceptedFiles, getRootProps, fileRejections, getInputProps } = useDropzone({
+        accept: {
+            'image/*': [],
+        },
         onDrop: (acceptedFiles) => {
             const file = acceptedFiles[0];
             const reader = new FileReader();
@@ -39,6 +48,18 @@ const ImageEditor = () => {
             reader.readAsDataURL(file);
         }
     });
+
+    function dataURLtoFile(dataurl, filename) {
+        var arr = dataurl.split(','),
+            mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[arr.length - 1]),
+            n = bstr.length,
+            u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8arr], filename, { type: mime });
+    }
 
     const drawRoundImage = (ctx, image, x, y, radius) => {
         return new Promise((resolve) => {
@@ -87,110 +108,164 @@ const ImageEditor = () => {
     };
 
 
-    const publicImages = [
-        {
-            name: 'Jai',
-            image: '/download.png'
-        },
-        {
-            name: 'Jai',
-            image: '/download.png'
-        },
-        {
-            name: 'Jai',
-            image: '/download.png'
-        },
-        {
-            name: 'Jai',
-            image: '/download.png'
-        },
-        {
-            name: 'Jai',
-            image: '/download.png'
-        },
-        {
-            name: 'Jai',
-            image: '/download.png'
-        },
-    ]
+    const onSubmit = (data) => {
+        if (isLoading) return
+        const regex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
+        if (!regex.test(data.email)) {
+            setError("email", {
+                type: "manual",
+                message: "Please enter a valid E-Mail address",
+            })
+            return
+        }
+        setIsLoading(true)
+        // converting base64 image to blob
+        const file = dataURLtoFile(previewImage, 'wellingtonImage.png')
+        const formData = new FormData()
 
+        formData.append('name', data.name)
+        formData.append('email', data.email)
+        formData.append('images', file)
+
+        axios.post(`${import.meta.env.VITE_API_URL}/game`, formData).then((res) => {
+            toast(res.data.message, {
+                style: {
+                    color: 'white',
+                    background: 'green',
+                },
+            })
+            setIsLoading(false)
+            handleClose()
+        }).catch(err => {
+            toast(`${err?.response?.data?.message ? err?.response?.data?.message : 'Server busy, please try again later'}`, {
+                style: {
+                    color: 'white',
+                    background: 'red',
+                },
+            })
+            setIsLoading(false)
+            handleClose()
+
+        }
+
+        )
+
+    }
+
+    useLayoutEffect(() => {
+        getData()
+    }, [])
 
 
 
     return (
-        <div className='flex flex-col gap-4'>
-            <div className='flex  flex-col gap-5 w-full pt-20 justify-center items-center'>
-                <h1 className='text-2xl font-semibold'>Wellington Sign Game</h1>
-                <div {...getRootProps({ className: 'dropzone w-[300px] h-[300px] font-medium flex justify-center items-center relative' })}>
-                    <input {...getInputProps()} />
-                    <p>Drag 'n' drop your image here, or click to select one</p>
+        <>
+            <Toaster />
+            <div className='flex flex-col gap-4 py-10 space-y-10'>
+                <h1 className='pacifico-font text-3xl md:text-4xl'>Wellington Sign Game</h1>
 
-                    {selfie && (
-                        <div className='absolute w-full h-full flex items-center'>
-                            <img src={selfie} className=' w-full rounded-md ' />
-                        </div>
-                    )
-                    }
+                <div className='flex  flex-col gap-5 w-full justify-center items-center'>
+                    <div className={`text-sm`}>Tip: If it fits the box below well, it will look better.</div>
+
+                    <div {...getRootProps({ className: 'dropzone w-[300px] h-[300px] font-medium flex justify-center items-center relative' })}>
+                        <input {...getInputProps()} />
+                        <p>Drag 'n' drop your image here, or click to select one</p>
+
+                        {selfie && (
+                            <div className='absolute flex items-center object-contain'>
+                                <img src={selfie} className='rounded-md object-contain max-w-[300px] max-h-[300px]' width={'300px'} height={'300px'} />
+                            </div>
+                        )
+                        }
+
+                    </div>
+
+                    <div className={`${!selfie && 'hidden'}  text-sm transition duration-300`}>Click on the box again to change selected image</div>
+                    <button
+                        onClick={createImage}
+                        className={`${!selfie && 'hidden'} rounded-md text-center px-4 py-2 bg-yellow-400 hover:bg-yellow-500 text-black hover:shadow-[0_0_0_1px#000000] transition duration-500`}>
+                        Start Generation
+                    </button>
 
                 </div>
-                <button onClick={createImage} className='rounded-md text-center px-4 py-2 bg-yellow-400 hover:bg-yellow-500 text-black hover:shadow-[0_0_0_1px#000000] transition duration-500'>Start Generation</button>
-            </div>
-            <div className='overflow-hidden w-full absolute -z-1'>
-                <canvas ref={canvasRef} width={1050} height={650} style={{ border: '1px solid black', display: "none" }} />
-            </div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <div className='relative w-[95vw] md:w-[70vw] bg-white shadow-[0_0_0_1px#ffdd00] rounded-md left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-3'>
-                    <div className='flex flex-col gap-3 justify-center items-center'>
-                        <div>Preview:</div>
-                        {previewImage && <img src={previewImage} alt="Preview" className='w-full md:w-[80%]' />}
+                <div className='overflow-hidden w-full absolute -z-1'>
+                    <canvas ref={canvasRef} width={1050} height={650} style={{ border: '1px solid black', display: "none" }} />
+                </div>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <div className='relative w-[95vw] md:w-[70vw] !h-[90vh] overflow-y-auto bg-white shadow-[0_0_0_1px#ffdd00] rounded-md left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-3'>
+                        <div className='flex flex-col gap-3 justify-center items-center'>
+                            <div>Your Preview:</div>
+                            {previewImage && <img src={previewImage} alt="Preview" className='w-full md:w-[80%]' />}
 
 
-                        <form class="w-full md:w-[60%] mx-auto py-4">
-                            <div className='text-center font-base text-base md:text-xl'>
-                                Enter your Details:
+                            <form class="w-full md:w-[60%] mx-auto py-4" onSubmit={handleSubmit(onSubmit)}>
+                                <div className='text-center font-base text-base'>
+                                    Enter your Details:
+                                </div>
+                                <div class="mb-5">
+                                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Name</label>
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5     "
+                                        placeholder="Full Name"
+                                        {...register('name', { requried: true })}
+                                        required
+                                    />
+                                </div>
+                                <div class="mb-5">
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">E-Mail:</label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5     "
+                                        placeholder="name@xyz.com"
+                                        {...register('email', { requried: true })}
+                                        required
+
+                                    />
+                                    {errors.email && <span className='pt-1 text-sm text-red-500'>{errors.email.message}</span>}
+                                </div>
+                                <div className='flex justify-center'>
+                                    <button type="submit" class="text-black bg-yellow-400 hover:bg-yellow-500 hover:shadow-[0_0_0_2px#000000] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">
+                                        {isLoading ? <ClipLoader color="#000000" /> : <>Submit</>}
+
+                                    </button>
+                                </div>
+                            </form>
+
+
+                        </div>
+                    </div>
+
+
+                </Modal>
+
+                <div className='flex flex-col gap-5 mt-4 '>
+                    <div className='w-full flex justify-center'>
+                        <span className='w-fit text-lg shadow-[0_3px#ffdd00] '>
+                            What other users generated:
+                        </span>
+                    </div>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 justify-center'>
+
+                        {publicImages && publicImages.map((item) => (
+                            <div className='rounded-md hover:-translate-y-1 transition duration-300'>
+                                <img src={item.image[0].url} alt={`${item.name} wellington sign image`} className='rounded-md shadow-[0_0_0_1px#ffff00] hover:shadow-[0_0_0_3px#ffff00]' />
+                                <div className='text-left text-sm italic p-1'>By {item.name}</div>
                             </div>
-                            <div class="mb-5">
-                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Your Name</label>
-                                <input type="text" id="name" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5     " placeholder="Full Name" required />
-                            </div>
-                            <div class="mb-5">
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                                <input type="email" id="email" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5     " placeholder="name@xyz.com" required />
-                            </div>
-                            <button type="submit" class="text-black bg-yellow-400 hover:bg-yellow-500 hover:shadow-[0_0_0_2px#000000] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
-                        </form>
-
-
+                        ))}
                     </div>
                 </div>
 
 
-            </Modal>
-
-            <div className='flex flex-col gap-5 mt-4 '>
-                <div className='w-full flex justify-center'>
-                    <span className='w-fit text-lg shadow-[0_3px#ffdd00] '>
-                        What other users generated:
-                    </span>
-                </div>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3'>
-
-                    {publicImages && publicImages.map((item) => (
-                        <div className='rounded-md   hover:-translate-y-1 transition duration-300'>
-                            <img src={item.image} alt={`${item.name} wellington sign image`} className='rounded-md shadow-[0_0_0_1px#ffff00] hover:shadow-[0_0_0_3px#ffff00]' />
-                            <div className='text-left text-sm italic p-1'>By {item.name}</div>
-                        </div>
-                    ))}
-                </div>
             </div>
-
-
-        </div>
+        </>
     );
 };
 
